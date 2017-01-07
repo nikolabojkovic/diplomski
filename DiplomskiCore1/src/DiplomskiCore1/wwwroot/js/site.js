185f844206a0e7fd1a52e9033c035b46a6384d04
Nikola Bojkovic <nikolabojkovic@gmail.com>
2017-01-07 20:37:19 +0100

    files re factor and nav-bar fixes

--- DiplomskiCore1/src/DiplomskiCore1/wwwroot/js/site.js
+++ DiplomskiCore1/src/DiplomskiCore1/wwwroot/js/site.js
@@ -80,6 +80,8 @@ else {
     $('#dislike').css('color', '');
 }
 
+// like - dislike functionality
+
 $("#like").on('click', function () {
     if (!like) {
         like = true;
@@ -152,3 +154,23 @@ $("#dislike").on('click', function () {
 $(function () {
     $('[data-toggle="popover"]').popover()
 });
+
+////////////// go to top button ///////////////////////
+
+var offset = 300;
+var duration = 500;
+jQuery(window).scroll(function () {
+    if (jQuery(this).scrollTop() > offset) {
+        jQuery('.back-to-top').fadeIn(duration);
+    } else {
+        jQuery('.back-to-top').fadeOut(duration);
+    }
+});
+
+jQuery('.back-to-top').click(function (event) {
+    event.preventDefault();
+    jQuery('html, body').animate({ scrollTop: 0 }, duration);
+    return false;
+});
+
+/////////////////////////////////////
